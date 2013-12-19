bf5383eecfb5ce071d986faefac37cd960674cfc
Stefan van Gastel <stefanvangastel@gmail.com>
2013-12-19 12:55:32 +0100

    Improved Mantis save method. Added HTTP auth options
    Added HTTP auth options to pass Kerberos for example. Also optional decode the image attachment data. Seems to differ in different versions of Mantis.

--- Model/Feedbackstore.php
+++ Model/Feedbackstore.php
@@ -42,32 +42,62 @@ class Feedbackstore extends AppModel {
 	Mantis store function
 	 */
 	public function mantis($feedbackObject = null){
-
+	
 		if(empty($feedbackObject)){
 			return false;
 		}
-
+	
+		//Mandatory
 		$api_url	= Configure::read('FeedbackIt.methods.mantis.api_url');
 		$username	= Configure::read('FeedbackIt.methods.mantis.username');
 		$password	= Configure::read('FeedbackIt.methods.mantis.password');
 		$project_id	= Configure::read('FeedbackIt.methods.mantis.project_id');
 		$category	= Configure::read('FeedbackIt.methods.mantis.category');
-
-		//Read mantis instance url from configfile
-		$c = new SoapClient($api_url);
-	   
-	    //Create new issue
-	    $issue = array ( 
-	                    'summary' => $feedbackObject['subject'], 
-	                    'description' => $feedbackObject['feedback'], 
-	                    'project'=>array('id'=>$project_id	), 
-	                    'category'=>$category
-	                    );
-	    if( $issueid = $c->mc_issue_add($username, $password, $issue) ){
-	    	//Add screenshot to issue (Do not send as base64 despite what de WSDL says)
-	   		return ($c->mc_issue_attachment_add( $username,  $password, $issueid, date('d-m-Y_H-i-s').'.png', 'image/png', base64_decode($feedbackObject['screenshot'])));
-	    }
-
-	    return false;
+		$decodeimage= Configure::read('FeedbackIt.methods.mantis.decodeimage');
+	
+		//Optional HTTP credentials for bypassing Basic Auth or Kerberos
+		$soap_options = array();
+	
+		if($http_username = Configure::read('FeedbackIt.methods.mantis.http_username') AND $http_password = Configure::read('FeedbackIt.methods.mantis.http_password') ){
+	
+			$soap_options = array(
+				'login'          => $http_username,
+				'password'       => $http_password,
+	
+				);
+		} 
+	
+		//Uncomment to debug:
+		$soap_options['cache_wsdl'] = WSDL_CACHE_NONE;
+	
+		//Create a SoapClient
+		$c = new SoapClient($api_url,$soap_options);
+	
+		//Mantis specific: append browser, browser version and URL to feedback:
+		$feedbackObject['feedback'] .= "\n\n";
+		$feedbackObject['feedback'] .= sprintf("Browser: %s %s\n",$feedbackObject['browser'],$feedbackObject['browser_version']);
+		$feedbackObject['feedback'] .= sprintf("Url: %s",$feedbackObject['url']);
+		
+	    	//Create new issue
+		$issue = array ( 
+			'summary' => $feedbackObject['subject'], 
+			'description' => $feedbackObject['feedback'], 
+			'project'=>array('id'=>$project_id), 
+			'category'=>$category
+			);
+	
+		//Try to save the issue
+		if( $issueid = $c->mc_issue_add($username, $password, $issue) ){
+			
+		    	//Decode image or not?
+			if($decodeimage){
+				$feedbackObject['screenshot'] = base64_decode($feedbackObject['screenshot']);
+			}
+	
+	    		//Add screenshot to issue (Do not send as base64 despite what de WSDL says)
+			return ($c->mc_issue_attachment_add( $username,  $password, $issueid, date('d-m-Y_H-i-s').'.png', 'image/png', $feedbackObject['screenshot'] ));
+		}
+	
+		return false;
 	}
 }
